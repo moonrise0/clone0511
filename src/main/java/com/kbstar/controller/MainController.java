8556d9de3eff64f0c834e4067f5791a59cc99903
junstdoitjun <bing74700@gmail.com>
2023-05-11 17:02:44 +0900

    center change

--- src/main/java/com/kbstar/controller/MainController.java
+++ src/main/java/com/kbstar/controller/MainController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
     @RequestMapping("/")
-    public String main() {
+    public String main(Model model) {
+        model.addAttribute("center", "center");
         return "index";
     }
 }
